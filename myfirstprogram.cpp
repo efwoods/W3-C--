5c8c6e57cca2f9da99ae556fd0e5681bad2afa94
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-10-19 03:08:10 -0400

    commit

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -52,9 +52,19 @@ int main()
     cout << a + b + z << endl;
 
     // One value to multiple values
-    int e,f,g;
+    int e, f, g;
     e = f = g = 50;
     cout << e + f + g << endl;
+
+    /*
+    The general rules for naming variables are:
+
+    Names can contain letters, digits and underscores
+    Names must begin with a letter or an underscore (_)
+    Names are case sensitive (myVar and myvar are different variables)
+    Names cannot contain whitespaces or special characters like !, #, %, etc.
+    Reserved words (like C++ keywords, such as int) cannot be used as names
+    */
 
     return 0;
 }
